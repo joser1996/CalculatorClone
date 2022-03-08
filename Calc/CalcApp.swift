26ea41d5a6e4a9930cb8bdf257fc33a6e62c247f
Jose Torres <josertrs@gmail.com>
2022-03-07 19:09:11 -0800

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by Jose Torres-Vargas on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
